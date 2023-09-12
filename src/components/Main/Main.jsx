184feb9a6c911d76c7db0642541d1b8b9dc9e16b
Cesaress <cesaress@yandex.ru>
2023-09-12 16:06:42 +0300

    Initial commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = (props) => {
   const location = useLocation();
 
   const classBased =
-    location.pathname === "/Diploma-FFE-Train-Tickets"
+    location.pathname === "/fe-diplom"
       ? "main-content__home-page"
       : "main-content__wrap d-flex";
   return (
