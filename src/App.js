0890f7c3f78ee4d3fa398d453cd7a62c77cf8ab3
Cesaress <cesaress@yandex.ru>
2023-10-21 01:11:08 +0300

    rebuild2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-import Layout from "/components/Layout";
-import HomePage from "/components/Pages/HomePage";
-import SelectionTrain from "/components/Pages/SelectionTrain";
-import SelectionWagons from "/components/Pages/SelectionWagons";
-import PassengersInfo from "/components/Pages/PassengersInfo";
-import PersonalData from "/components/Pages/PersonalData";
-import Screening from "/components/Pages/Screening";
-import OrderResult from "/components/Pages/OrderResult";
-import NotFound from "/components/Pages/NotFound";
+import Layout from "src/components/Layout";
+import HomePage from "src/components/Pages/HomePage";
+import SelectionTrain from "src/components/Pages/SelectionTrain";
+import SelectionWagons from "src/components/Pages/SelectionWagons";
+import PassengersInfo from "src/components/Pages/PassengersInfo";
+import PersonalData from "src/components/Pages/PersonalData";
+import Screening from "src/components/Pages/Screening";
+import OrderResult from "src/components/Pages/OrderResult";
+import NotFound from "src/components/Pages/NotFound";
 import "App.css";
 
 function App() {
