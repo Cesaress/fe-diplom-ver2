2f0a3b6b7ba23278a7b5e8de081b607414db6658
Cesaress <cesaress@yandex.ru>
2023-10-21 15:05:25 +0300

    new_rebuild7

--- src/components/Pages/OrderResult.jsx
+++ src/components/Pages/OrderResult.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import Banner from "../Molecules/Banner";
 import ControlBlockFeedBack from "../Main/OrderResult/ControlBlockFeedBack";
 import banner_order_page from "../../img/banner/banner_order_page.png";
-import Title from "../Atoms/Atoms";
+import { Title } from "../Atoms/Atoms.jsx";
 import Card from "../Main/CardsBlock/Card";
 import {
   CardTop,
